cf3e0059a68e3327db4aacdd9608fe46f89637f2
ecotank <email.anda@domain.com>
2026-07-20 09:43:27 +0700

    Fix headless Chrome mode and workflow runner for GitHub Actions CI/CD

--- test_app.py
+++ test_app.py
@@ -37,7 +37,7 @@ class TestLoginRegister(unittest.TestCase):
 
         print("[+] Membuka Selenium Chrome Headless Driver...", flush=True)
         chrome_options = Options()
-        # chrome_options.add_argument("--headless=new")
+        chrome_options.add_argument("--headless=new")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--disable-gpu")
